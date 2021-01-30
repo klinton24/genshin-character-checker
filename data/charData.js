41fc71b272a87c70f82ac9a90069ed473f398678
K Smith <klinton.jp@gmail.com>
2021-01-30 06:07:09 -0700

    My First Commit

---
+++ data/charData.js
@@ -0,0 +1,12 @@
+export const charData = [
+    {
+      charName: "Zhongli",
+      lvl20Uncap: [[[1]["Prithiva Topaz Sliver"]][[3]['Cor Lapis']]],
+      lvl40Uncap: [[[3]['Prithiva Topaz Fragment']][[10]['Cor Lapis']]] 
+    },
+    {
+      charName: "Diluc",
+      lvl20Uncap: { "Agnidus Agate Sliver": 1, "Small Lamp Grass": 3 },
+      lvl40Uncap: { "Agnidus Agate Fragment": 3, "Small Lamp Grass": 10}
+    }
+  ]
