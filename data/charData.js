a41be6a8ef8db841ed7611fe38f2e1cc83d740d3
Klinton Smith <klinton.jp@gmail.com>
2021-02-04 19:14:38 -0700

    Updated Array

--- data/charData.js
+++ data/charData.js
@@ -1,13 +1,36 @@
-export const charData = [
+let gemMat;
+let gatherMat;
+let bossMat;
+let monsterMat;
+let mora;
+
+const charData = [
     {
       charName: "Zhongli",
-      lvl20Uncap: [[[1]["Prithiva Topaz Sliver"]][[3]['Cor Lapis']]],
-      lvl40Uncap: [[[3]['Prithiva Topaz Fragment']][[10]['Cor Lapis']]] 
+      lvl20Uncap: { "Agnidus Agate Sliver": 1, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 20000},
+      lvl40Uncap: { "Agnidus Agate Sliver": 9, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 20000},
+      lvl50Uncap: { "Agnidus Agate Sliver": 18, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 20000},
+      lvl60Uncap: { "Agnidus Agate Sliver": 27, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 20000},
+      lvl70Uncap: { "Agnidus Agate Sliver": 54, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 20000},
+      lvl80Uncap: { "Agnidus Agate Sliver": 162, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 20000},
     },
     {
       charName: "Diluc",
-      lvl20Uncap: { "Agnidus Agate Sliver": 1, "Small Lamp Grass": 3 },
-      lvl40Uncap: { "Agnidus Agate Fragment": 3, "Small Lamp Grass": 10}
+      lvl20Uncap: { "Agnidus Agate Sliver": 1, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 20000},
+      lvl40Uncap: { "Agnidus Agate Sliver": 9, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 40000},
+      lvl50Uncap: { "Agnidus Agate Sliver": 18, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 60000},
+      lvl60Uncap: { "Agnidus Agate Sliver": 27, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 80000},
+      lvl70Uncap: { "Agnidus Agate Sliver": 54, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 100000},
+      lvl80Uncap: { "Agnidus Agate Sliver": 162, "Everflame Seed": 2, "Small Lamp Grass": 3, "Slime Condensate": 3, "Mora": 120000},
+    },
+    {
+      charName: "Generic",
+      lv20Uncap: {gemMat: 1, gatherMat: 3, monsterMat:3, mora: 20000 },
+      lv40Uncap: {gemMat: 9, bossMat: 2, gatherMat: 10, monsterMat:3, mora: 40000 },
+      lv50Uncap: {gemMat: 18, bossMat: 4, gatherMat: 20, monsterMat:3, mora: 60000 },
+      lv60Uncap: {gemMat: 27, bossMat: 8, gatherMat: 30, monsterMat:3, mora: 80000 },
+      lv70Uncap: {gemMat: 54, bossMat: 12, gatherMat: 45, monsterMat:3, mora: 100000 },
+      lv80Uncap: {gemMat: 162, bossMat: 20, gatherMat: 60, monsterMat:3, mora: 120000 }
     }
   ]
 
